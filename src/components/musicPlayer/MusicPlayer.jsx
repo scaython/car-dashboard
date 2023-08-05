444727ec64aca2fcbc45f1e8523fcd61ee3f3f01
scaython <ma.enes@hotmail.com>
2023-08-06 01:16:02 +0300

    Add music player(Skeleton)

---
+++ src/components/musicPlayer/MusicPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MusicPlayer.scss'
+const MusicPlayer = () => {
+  return (
+    <div className='widget-wrapper'>
+
+    </div>
+  )
+}
+
+export default MusicPlayer
